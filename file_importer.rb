e5120e716e5ce128ff88ef742336463864e87755
Joe Sturzenegger <joesturz84@gmail.com>
2018-08-14 19:48:07 -0600

    refactored FileImporter into a static class

--- file_importer.rb
+++ file_importer.rb
@@ -4,18 +4,20 @@ require 'open-uri'
 class FileImporter
 
   attr_accessor :file
-  
-  def initialize(file_name)
-    @path = File.join(File.dirname(__FILE__), file_name)
-    @file = File.open(@path)
+
+  def self.split_on_line(file_name)
+    file = open_file(file_name)
+    file.read.split("\n")
   end
 
-  def split_on_line
-    @file.read.split("\n")
+  def self.split_on_line_from_link(url)
+    file = open(url).read
+    file.split("\n")
   end
 
-  def get_hash
-    json_file = File.read(@path)
+  def self.get_hash(file_name)
+    file = open_file(file_name)
+    json_file = File.read(file)
     JSON.parse(json_file)
   end
 
@@ -24,4 +26,9 @@ class FileImporter
     JSON.parse(json_file)
   end
 
+  private
+  def self.open_file(file_name)
+    path = File.join(File.dirname(__FILE__), file_name)
+    File.open(path)
+  end
 end
--- spec/file_importer_spec.rb
+++ spec/file_importer_spec.rb
@@ -2,31 +2,33 @@ require 'spec_helper'
 
 describe FileImporter do
 
-  let(:file_importer) {FileImporter.new('spec/fixtures/test_file.txt')}
-  let(:json_file_importer) {FileImporter.new('spec/fixtures/json_test_file.txt')}
-
-
-  it 'can open a file' do
-    expect(file_importer.file).to_not be_nil
-  end
+  let(:file) {'spec/fixtures/test_file.txt'}
+  let(:json_file) {'spec/fixtures/json_test_file.txt'}
+  let(:remote_json_file) {'http://mysafeinfo.com/api/data?list=englishmonarchs&format=json'}
 
   context 'reading lines' do
 
     it 'can read the first line of a file' do
-      lines = file_importer.split_on_line
+      lines = FileImporter.split_on_line(file)
       expect(lines.first).to eq 'This is a test'
     end
 
     it 'can read the last line of a file' do
-      lines = file_importer.split_on_line
+      lines = FileImporter.split_on_line(file)
       expect(lines.last).to eq 'This is a second test'
+    end
+    context 'Remote' do
+      it 'can get the lines of a remote file' do
+        lines = FileImporter.split_on_line_from_link(remote_json_file)
+        expect(lines.count).to eq 344
+      end
     end
 
   end
 
   context 'JSON' do
     it 'can read in JSON from a file and return a hash' do
-      json_hash = json_file_importer.get_hash
+      json_hash = FileImporter.get_hash(json_file)
       expect(json_hash['name']).to eq 'Homer Simpson'
       expect(json_hash['city']).to eq 'Springfield'
       expect(json_hash['state']).to eq 'Unknown'
@@ -34,12 +36,14 @@ describe FileImporter do
       expect(json_hash['family']).to match_array(["Marge", "Bart", "Lisa", "Maggie"])
       expect(json_hash['year']).to eq 1989
     end
+
+    context 'Remote' do
+      it 'can get data from a remote json file and return a hash' do
+        json_hash = FileImporter.get_hash_from_link(remote_json_file)
+        expect(json_hash.count).to eq 57
+      end
+    end
+
   end
 
-  context 'Remote JSON files' do
-    it 'can get data from a remote json file' do
-      json_hash = FileImporter.get_hash_from_link('http://mysafeinfo.com/api/data?list=englishmonarchs&format=json')
-      expect(json_hash.count).to eq 57
-    end
-  end
 end
